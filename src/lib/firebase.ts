b194b253df173c37d7c3952b2c7610efdf0ae161
unknown <jameskolleh@gmail.com>
2026-04-06 15:48:17 +0000

    Initial commit with web build

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
-  projectId: 'YOUR_PROJECT_ID',
-  storageBucket: 'YOUR_PROJECT_ID.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyDHlKPCL3KD4yHG3pBGRQmIIMH3msd3YX8",
+  authDomain: "dealspot-9da30.firebaseapp.com",
+  projectId: "dealspot-9da30",
+  storageBucket: "dealspot-9da30.firebasestorage.app",
+  messagingSenderId: "450957608071",
+  appId: "1:450957608071:web:ca20196483c32c2fa4e422",
+  measurementId: "G-13RHQFSCMS"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
